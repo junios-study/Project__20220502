982dcdee45dac0e78835c41bced36f17961ffc77
junios <junios@naver.com>
2022-05-02 10:30:28 +0900

    float 연산

--- main.cpp
+++ main.cpp
@@ -12,9 +12,17 @@ int main()
 	cout << MyMoney - Operand << endl;
 	cout << MyMoney / Operand << endl;
 	cout << MyMoney % Operand << endl;
+	cout << 10 + (2 * 20) << endl;
 
-	cout << 10 + 2 * 20 << endl;
+	float MyR = 0.1f;
+	float MyG = 0.3f;
+	float MyB = 0.2f;
 
+	float GrayColor = (0.299f * MyR) + (0.587f * MyG) 
+		+ (0.114f * MyB);
+
+
+	cout << GrayColor << endl;
 
 	return 0;
 }
