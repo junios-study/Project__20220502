6229f0a77571f9c28d389388cfe4fbb66f3967d5
junios <junios@naver.com>
2022-05-02 14:32:05 +0900

    홀수의 합 짝수의 합

--- main.cpp
+++ main.cpp
@@ -5,12 +5,21 @@ using namespace std;
 int main()
 {
 	int Sum = 0;
-	for (int i = 1; i <= 100; i++)
+	for (int i = 1; i <= 100; i += 2)
 	{
 		Sum += i;
 	}
 
 	cout << Sum << endl;
+
+	Sum = 0;
+	for (int i = 2; i <= 100; i += 2)
+	{
+		Sum += i;
+	}
+
+	cout << Sum << endl;
+
 
 
 	return 0;
